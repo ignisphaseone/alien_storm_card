8fa53c0cac0ed52388cf7ad69fde89dc7d027b96
Eric Fong <emfong@ignisphaseone.com>
2012-06-20 01:00:38 -0700

    dice has more functions, still sample code.

--- src/alien_storm/data/dice.py
+++ src/alien_storm/data/dice.py
@@ -41,11 +41,10 @@ print get_die("basic_attack").sides
 print get_die("civilian").sides
 
 def roll_all(dice):
-    mydice = dice
-    for die in mydice:
+    for die in dice:
         die.roll()
     while True:
-        for die in mydice:
+        for die in dice:
             print die.faceup
         icmd = raw_input("--reroll? ")
         if icmd == "done":
@@ -53,11 +52,23 @@ def roll_all(dice):
         else:
             try:
                 cmd = int(icmd)
-                if cmd <= mydice.__len__() and cmd > 0:
-                    mydice[cmd-1].roll()
+                if cmd <= dice.__len__() and cmd > 0:
+                    dice[cmd-1].roll()
                 else:
                     print "--invalid value (out of bounds)..."
             except ValueError:
                 print "--invalid value (not an int)..."
-
+    results = {}
+    faces = []
+    for die in dice:
+        if (die.faceup is not None):
+            faces.append(die.faceup)
+    for face in faces:
+        if not results.has_key(face[0]):
+            results[face[0]] = face[1]
+        else:
+            results[face[0]] += face[1]
+    print results
+    return results
+        
 roll_all(dice_list)
