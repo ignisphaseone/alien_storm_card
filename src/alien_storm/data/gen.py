56a48120868e3c98d2c49989844d43517fd9b005
Eric Fong <emfong@ignisphaseone.com>
2012-07-02 23:15:00 -0700

    began generation of dice, so far basic-attack is the only coded die

--- src/alien_storm/data/gen.py
+++ src/alien_storm/data/gen.py
@@ -1,6 +1,17 @@
-'''
-Created on Jun 18, 2012
+import json, dice
+from dice import die, add_to_all_dice
 
-@author: ApigeeCorporation
-'''
-import json
+json_die = {
+    "name":"basic-attack",
+    "color":"red",
+    "sides":[
+        {"attack":1},
+        {"attack":1},
+        {"attack":1},
+        {"attack":2},
+        {"attack":2},
+        {"miss":1},
+        ],
+    }
+new_die = die(json.dumps(json_die))
+add_to_all_dice(new_die)
